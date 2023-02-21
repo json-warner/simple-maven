cd6308ad63d51cecbf640677d9c25051056d8efb
Jason Warner <json.warner@gmail.com>
2023-02-21 01:29:01 -0500

    spring initializer

---
+++ src/main/java/net/thewarnerhome/simple/SimpleMavenApplication.java
@@ -0,0 +1,13 @@
+package net.thewarnerhome.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/thewarnerhome/simple/SimpleMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package net.thewarnerhome.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
